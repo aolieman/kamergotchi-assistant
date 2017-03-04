600e1c1a05df16f4e2b3ce23234620d9406440cf
Alex Olieman <alex@olieman.net>
2017-03-04 23:21:58 +0100

    be more agressive about claiming the bonus asap

--- kg-py3.py
+++ kg-py3.py
@@ -19,6 +19,7 @@ import logging
 from numpy.random import lognormal
 from pprint import pprint
 from functools import wraps
+from operator import itemgetter
 
 from secret import PLAYER_ID, SLEEP_INTERVAL
 
@@ -71,49 +72,35 @@ def getInfo(player_token):
 
     
 def giveMostNeededCare(player_token):
-    returnJson = json.loads(getInfo(player_token))
-    game = returnJson['game']
-    careLeft = game['careLeft']
+    return_json = json.loads(getInfo(player_token))
+    game = return_json['game']
+    care_left = game['careLeft']
     current = game['current']
     full_health = min(current.values()) == 100
 
-    foodValue = current['food']
-    attentionValue = current['attention']
-    knowledgeValue = current['knowledge']
+    care_reset = game['careReset']
+    claim_reset = game['claimReset']
+    care_reset_date = datetime.datetime.strptime(care_reset, "%Y-%m-%dT%H:%M:%S.%fZ")
+    claim_reset_date = datetime.datetime.strptime(claim_reset, "%Y-%m-%dT%H:%M:%S.%fZ")
+    utcnow = datetime.datetime.utcnow()
 
-    careReset = game['careReset']
-    claimReset = game['claimReset']
-
-    careResetDate = datetime.datetime.strptime(careReset, "%Y-%m-%dT%H:%M:%S.%fZ")
-    claimResetDate = datetime.datetime.strptime(claimReset, "%Y-%m-%dT%H:%M:%S.%fZ")
-    now = datetime.datetime.utcnow()
-
-    # check if there are cares left, or if the careResetDate has elapsed (careLeft stays 0 even after reset date)
+    # claim bonus, or give care, or wait for a while
     wait_seconds = 0
-    if not full_health and (careLeft > 0 or now > careResetDate):
-
-        if (foodValue < attentionValue):
-            if (knowledgeValue < foodValue):
-                giveCare(player_token, 'knowledge');
-            else:
-                giveCare(player_token, 'food')
-            
-        else:
-            if (attentionValue < knowledgeValue):
-                giveCare(player_token, 'attention')
-            else:
-                giveCare(player_token, 'knowledge')
-    elif (now > claimResetDate):
-        # time to claim the bonus
+    if full_health and (now > claim_reset_date):
         claimBonus(player_token)
         pprint(game)
+    elif not full_health and (care_left > 0 or now > care_reset_date):
+        lowest_stat = min(current.items(), key=itemgetter(1))[0]
+        giveCare(player_token, lowest_stat)
     elif full_health:
+        # wait until next action
         wait_seconds = 1
     else:
-        wait_seconds = (careResetDate-now).total_seconds()
+        # we hit the care limit; try again in about 6 minutes
+        wait_seconds = (careResetDate - utcnow).total_seconds()
         progress('Not yet! Remaining seconds: {}'.format(wait_seconds))
         
-    return wait_seconds
+    return wait_seconds, claim_reset_date
     
 
 @timed_action
@@ -194,12 +181,16 @@ def ceil_dt(dt, delta):
     return dt + (datetime.datetime.min - dt) % delta
     
     
-def get_next_dt():
-    next_half_hour = ceil_dt(datetime.datetime.utcnow(), datetime.timedelta(minutes=30))
+def get_next_dt(claim_reset_date=None):
+    utcnow = datetime.datetime.utcnow()
+    next_half_hour = ceil_dt(utcnow, datetime.timedelta(minutes=30))
     if next_half_hour.minute == 0:
-        return next_half_hour + datetime.timedelta(minutes=30)
-    else:
-        return next_half_hour
+        next_half_hour += datetime.timedelta(minutes=30)
+    
+    if claim_reset_date and utcnow < claim_reset_date < next_half_hour:
+        return claim_reset_date
+        
+    return next_half_hour
         
 
 if __name__ == '__main__':
@@ -219,9 +210,10 @@ if __name__ == '__main__':
                 short_intervals = lognormal(0, 1, size=30) / 2
                 
                 wait = 0
+                claim_reset = next_dt
                 for siv in short_intervals:
                     try:
-                        wait = giveMostNeededCare(player_token)
+                        wait, claim_reset = giveMostNeededCare(player_token)
                         if wait:
                             break
                     except (HTTPError, URLError):
@@ -231,11 +223,13 @@ if __name__ == '__main__':
                     time.sleep(wait + siv)
                 
                 utcnow = datetime.datetime.utcnow()
+                
+                # keep trying until an action is taken or 9 minutes have passed
                 if wait is 1 and (
                     (last_action_time > next_dt)
                     or (utcnow > next_dt + datetime.timedelta(minutes=9))
                 ):
-                    next_dt = get_next_dt()
+                    next_dt = get_next_dt(claim_reset)
                     sleep_until(next_dt, liv)
                 else:
                     snooze = wait + min(liv, 367.67)
